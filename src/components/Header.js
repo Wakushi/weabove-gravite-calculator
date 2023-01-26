ad8b06ff8b4dbcfa23067b439520399c36746705
Wakushi <mzozord@gmail.com>
2023-01-26 10:39:55 +0100

    birthday message

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,10 @@ export default function Header() {
         <header>
             
                 <a target='_blank' href="https://weabove.io/"><img className="main-logo" src={weabove} width="200px"></img></a>
-
-                <h1>GRAVITE <em>CALCULATOR</em></h1>
-                
+                <div className="header-title">
+                    <h1>GRAVITE <em>CALCULATOR</em></h1>
+                    <h1>Happy Birthday <em>Gahk</em> !</h1>
+                </div>
                 <a target="_blank" href="https://opensea.io/collection/weabove-prime"><img className="os-logo" src={gravite} alt="opensea" width="80px"></img></a>
           
         </header>
