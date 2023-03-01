f21c873cc7a27737981b536e1ebba90daf80ce4a
Wakushi <mzozord@gmail.com>
2023-03-01 13:23:37 +0100

    changed header link to loyal3

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 import weabove from "../assets/weabove.png"
 import gravite from "../assets/opensea.png"
 
-export default function Header(props) {
+export default function Header() {
     return(
         <header>
             
-                <a target='_blank' href="https://weabove.io/"><img className="main-logo" src={weabove} width="110px"></img></a>
+                <a target='_blank' href="https://www.loyal3program.io/"><img className="main-logo" src={weabove} width="110px"></img></a>
                 <div className="header-title">
                     <h1>GRAVITE <em>CALCULATOR</em></h1>
                 </div>
